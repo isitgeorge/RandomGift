5cbf08bfb49ad2923cfcd2edfe212904d2166891
George <isitgeorge@gmail.com>
2013-10-22 00:14:42 +0100

    Initial files

---
+++ src/com/isitgeo/randomgift/RandomGift.java
@@ -0,0 +1,64 @@
+package com.isitgeo.randomgift;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RandomGift extends JavaPlugin implements Listener {
+
+	private File config;
+	private Player player;
+	private RandomGiftGen rGG;
+	public long cooldown;
+	public int cooldownTime;
+	public String[] gList;
+
+	@Override
+	public void onEnable() {
+
+		config = new File(getDataFolder(), "config.yml");
+
+		if (!(config.exists())) {
+			getLogger().info("Configuration not found...");
+			this.saveDefaultConfig();
+			getLogger().info("Created configuration!");
+		}
+		load();
+		getServer().getPluginManager().registerEvents(this, this);
+
+		try {
+		    MetricsLite metrics = new MetricsLite(this);
+		    metrics.start();
+		} catch (IOException e) {
+		    // Failed to submit the stats :-(
+		}
+		
+		getLogger().info("This plugin collects statistic data and sends it to http://mcstats.org/plugin/RandomGift");
+		getLogger().info("You can opt-out by editing the PluginMetrics configuration located in the plugins folder.");
+		getLogger().info("RandomGift enabled successfully!");
+	}
+
+	public void load() {
+		gList = this.getConfig().getStringList("items").toArray(new String[0]);
+		cooldownTime = this.getConfig().getInt("cooldown-time") * 60 * 1000;
+		cooldown = 0;
+		rGG = new RandomGiftGen(this);
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("RandomGift disabled successfully!");
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) throws IOException {
+		player = event.getPlayer();
+		rGG.check(player);
+	}
+
+}
---
+++ src/com/isitgeo/randomgift/RandomGiftGen.java
@@ -0,0 +1,84 @@
+package com.isitgeo.randomgift;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class RandomGiftGen {
+
+	private RandomGift plugin;
+
+	public RandomGiftGen(RandomGift plugin) {
+		this.plugin = plugin;
+	}
+
+	public void check(Player player) throws IOException {
+
+		if (System.currentTimeMillis() - plugin.cooldown >= plugin.cooldownTime) {
+			if (player.hasPermission("randomgift.trigger")) {
+				getPlayers(player);
+				plugin.cooldown = System.currentTimeMillis();
+			} else {
+				return;
+			}
+
+		}
+	}
+
+	public void getPlayers(Player player) {
+		Player[] pList = plugin.getServer().getOnlinePlayers();
+
+		if (pList.length < plugin.getConfig().getInt("minimum-players")) {
+			return;
+		}
+
+		Random pSelect = new Random();
+		int pRand = pSelect.nextInt(pList.length);
+
+		Player rPlayer = pList[pRand];
+		plugin.getServer().broadcastMessage(
+				ChatColor.GOLD + "[RandomGift] " + ChatColor.WHITE
+						+ rPlayer.getName() + " has been given a random gift!");
+
+		rPlayer.sendMessage(ChatColor.GOLD + "[RandomGift] " + ChatColor.WHITE
+				+ "Be sure to thank " + player.getName()
+				+ " for your random gift!");
+		generateGift(rPlayer);
+	}
+
+	public void generateGift(Player rPlayer) {
+
+		Random gSelect = new Random();
+		int gRand = gSelect.nextInt(plugin.gList.length);
+
+		String[] itemQuant = plugin.gList[gRand].split(" ");
+
+		int itemQuantity = Integer.parseInt(itemQuant[1]);
+
+		String[] itemDV;
+
+		if (!(itemQuant[0].contains(":"))) {
+
+			int itemNumber = Integer.parseInt(itemQuant[0]);
+
+			rPlayer.getInventory().addItem(
+					new ItemStack(Material.getMaterial(itemNumber),
+							itemQuantity));
+
+		} else {
+			itemDV = itemQuant[0].split(":");
+
+			int itemNumber = Integer.parseInt(itemDV[0]);
+			int itemDataV = Integer.parseInt(itemDV[1]);
+
+			rPlayer.getInventory().addItem(
+					new ItemStack(Material.getMaterial(itemNumber),
+							itemQuantity, (short) itemDataV));
+		}
+	}
+
+}
