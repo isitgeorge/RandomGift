d0edfd2e8521803d7b54602efc4aafc3e1d62694
FearFree <fearfree@cubetown.net>
2013-10-25 07:58:09 -0500

    Break the loop

--- src/com/isitgeo/randomgift/RandomGiftGen.java
+++ src/com/isitgeo/randomgift/RandomGiftGen.java
@@ -45,10 +45,12 @@ public class RandomGiftGen {
 				ChatColor.GOLD + "[RandomGift] " + ChatColor.WHITE
 						+ rPlayer.getName() + " has been given a random gift!");
 
-		    rPlayer.sendMessage(ChatColor.GOLD + "[RandomGift] " + ChatColor.WHITE
-				+ "Be sure to thank " + player.getName()
-				+ " for your random gift!");
-		    generateGift(rPlayer);
+		        rPlayer.sendMessage(ChatColor.GOLD + "[RandomGift] " + ChatColor.WHITE
+				        + "Be sure to thank " + player.getName()
+				        + " for your random gift!");
+		        generateGift(rPlayer);
+			
+			    break;
 	        }
 	    }
 	}
